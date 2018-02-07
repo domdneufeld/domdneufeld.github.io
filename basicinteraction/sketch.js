e33446668d7b8dea4b69776daa56832166ab4dff
domdneufeld <domdneufeld@gmail.com>
2018-02-07 15:17:45 -0600

    Created Basic Interaction

--- basicinteraction/sketch.js
+++ basicinteraction/sketch.js
@@ -1,34 +1,37 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
+// basic interaction
+// Dom Neufeld
+// February 7, 2018
 
 // global variables
-let gear;
-
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
+let click;
+let keyPress;
 // the setup function will only run once (before the draw loop begins)
 // this is where you want to set up the environment (size of canvas, etc)
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(255);
 
-  image(gear, 0, 0);
+}
 
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
+function keyPressed() {
   noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  ellipse(random(width), random(height), random(40, 100), random(40, 100));
 
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+  textSize(32);
+  text('word', random(width), random(height));
+}
+
+function mouseClicked() {
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  rect(mouseX, mouseY, random(40, 100), random(40, 100));
+}
+
+function deviceShaken() {
+  textSize(32);
+  text('word', random(width), random(height));
 }
