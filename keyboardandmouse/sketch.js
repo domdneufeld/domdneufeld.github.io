174b487fb6558adac4cfc02ebaf5d394e8113bb5
domdneufeld <domdneufeld@gmail.com>
2018-02-08 14:47:00 -0600

    gave myself practice questions

--- keyboardandmouse/sketch.js
+++ keyboardandmouse/sketch.js
@@ -1,34 +1,8 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
+// Keyboard and Mouse
+// Dom Neufeld
+// February 8th, 2018
 
-// global variables
-let gear;
-
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(255);
-
-  image(gear, 0, 0);
-
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
-  noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
-}
+// left mouse button draw a rectangle there
+// Right mouse button draw a circle there
+// w key reset sketch with white background
+// b key reset sketch with black background 
