dc15e2bbd96b3b994500a8c945d7fdaae486008b
domdneufeld <domdneufeld@gmail.com>
2018-02-14 15:20:04 -0600

    made colour slider

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -4,32 +4,67 @@
 
 
 // Global Variables
-let xSpeed = 0;
-let ySpeed = 0;
-let x = 1000;
-let y = 250;
+let sphereX;
+let sphereY;
+
+let movingLeft;
+let movingRight;
+
+let colour;
+
+let sphereSize = 50;
 
 function setup() {
-  createCanvas(windowWidth - 20, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  sphereX = width / 2;
+  sphereY = 650;
 }
 
 function draw() {
   background(255);
+  // Constrains movement of the sphere
+  sphereX = constrain(sphereX,175,1425);
 
-  x = x + xSpeed
-  
-  if (keyIsDown(RIGHT_ARROW)) {
-    xSpeed = 5;
-  } else if (keyIsDown(LEFT_ARROW)) {
-    xSpeed = -5;
-  } else {
-    xSpeed = 0;
+  // Moves the sphere to left and right
+  if (movingLeft) {
+    sphereX -= 5;
+  }
+  if (movingRight) {
+    sphereX += 5;
   }
 
-  drawSphere(x, y);
+  changeColour(sphereX);
+  drawSphere(sphereX, sphereY, colour);
 }
 
-function drawSphere(x, y) {
-  fill(0, 0, 255);
-  ellipse(x, y, 100, 100);
+function drawSphere(x, y, colour) {
+  // draws the sphere which displays the colour selected
+  fill(abs(colour - 250), 0, colour);
+  ellipse(x, y, sphereSize, sphereSize);
+}
+
+function keyPressed() {
+  if (key === "a" || key === "A") {
+    movingLeft = true;
+  }
+  if (key === "d" || key === "D") {
+    movingRight = true;
+  }
+}
+
+function keyReleased() {
+  if (key === "a" || key === "A") {
+    movingLeft = false;
+  }
+  if (key === "d" || key === "D") {
+    movingRight = false;
+  }
+}
+
+
+
+function changeColour(sphereX) {
+  //middle of the screen is 800.
+  // right max makes colour 0 and left max makes colour 250
+  colour = (1425 - sphereX)/5
 }
