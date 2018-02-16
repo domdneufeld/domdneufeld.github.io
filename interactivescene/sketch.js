0e48f76c20d4ee112de7d398c681a855eaa45b7a
domdneufeld <domdneufeld@gmail.com>
2018-02-16 15:20:08 -0600

    added triangles

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -12,18 +12,19 @@ let movingRight;
 
 let colour;
 
-let sphereSize = 50;
+let shapeChoice;
+
+let sphereSize = 100;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   sphereX = width / 2;
-  sphereY = 650;
+  sphereY = 700;
 }
 
 function draw() {
-  background(255);
   // Constrains movement of the sphere
-  sphereX = constrain(sphereX,175,1425);
+  sphereX = constrain(sphereX, 175, 1425);
 
   // Moves the sphere to left and right
   if (movingLeft) {
@@ -33,8 +34,15 @@ function draw() {
     sphereX += 5;
   }
 
+  // makes background over colour slider black
+  fill(0);
+  rect(0, 620, width, 880)
+
   changeColour(sphereX);
   drawSphere(sphereX, sphereY, colour);
+  if (mouseIsPressed) {
+    drawRandomShape(mouseX, mouseY, colour);
+  }
 }
 
 function drawSphere(x, y, colour) {
@@ -44,6 +52,7 @@ function drawSphere(x, y, colour) {
 }
 
 function keyPressed() {
+  // recognizes when left or right arrows are down
   if (key === "a" || key === "A") {
     movingLeft = true;
   }
@@ -53,6 +62,7 @@ function keyPressed() {
 }
 
 function keyReleased() {
+  // recognizes when left or right arrows are released
   if (key === "a" || key === "A") {
     movingLeft = false;
   }
@@ -65,6 +75,17 @@ function keyReleased() {
 
 function changeColour(sphereX) {
   //middle of the screen is 800.
-  // right max makes colour 0 and left max makes colour 250
-  colour = (1425 - sphereX)/5
+  //right max makes colour 0 and left max makes colour 250
+  //divided by five because the ball moves in intervals of 5
+  colour = (1425 - sphereX) / 5
+}
+
+function drawRandomShape(x, y, colour) {
+  y = constrain(y, 0, 500)
+  shapeChoice = random(2,5)
+
+  if (shapeChoice === 3) {
+  fill(abs(colour - 250), 0, colour);
+  triangle(x, y, x + random(10, 100), y + random(0, 10), x + random(20, 120), y + random(20, 120));
+  }
 }
