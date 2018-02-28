12ad645a6b550a2c5138dd2f7b69d1bf1018c47b
domdneufeld <domdneufeld@gmail.com>
2018-02-28 15:17:31 -0600

    tried

--- dvdbounce/sketch.js
+++ dvdbounce/sketch.js
@@ -6,6 +6,7 @@
 let x, y, radius;
 let dx, dy;
 let dvd, dvdColor;
+let state;
 
 function preload() {
   dvd = loadImage("images/logo.png");
@@ -19,12 +20,20 @@ function setup() {
   dx = random(2, 5);
   dy = random(2, 5);
   dvdColor = color(0);
+  state = 1;
 }
 
 function draw() {
   background(255);
-  moveThing();
-  displayThing();
+
+  if (state === 1) {
+    displayButton();
+  }
+
+  if (state === 2) {
+    moveThing();
+    displayThing();
+  }
 }
 
 function moveThing() {
@@ -32,12 +41,12 @@ function moveThing() {
   y += dy;
 
   //check if bounce required
-  if (y + dvd.height/2 >= height || y - dvd.height/2 <= 0) {
+  if (y + dvd.height / 2 >= height || y - dvd.height / 2 <= 0) {
     dy = dy * -1;
     dvdColor = color(random(255), random(255));
   }
 
-  if (x + dvd.width/2 >= width || x - dvd.width/2 <= 0) {
+  if (x + dvd.width / 2 >= width || x - dvd.width / 2 <= 0) {
     dx = dx * -1;
     dvdColor = color(random(255), random(255));
   }
@@ -49,4 +58,23 @@ function displayThing() {
   imageMode(CENTER);
   tint(dvdColor);
   image(dvd, x, y);
+}
+
+function displayButton() {
+  fill(255, 0, 0);
+  rect(width / 2 - 100, height / 2 - 50, 200, 100);
+
+  textSize(24);
+  fill(0);
+  text("Play", width / 2 - 25, height / 2 + 10);
+}
+
+function mouseClicked(){
+  checkButton();
+}
+
+function checkButton() {
+  if (width / 2 - 100 <= mouseX <= width / 2 + 100 && height / 2 - 50 <= mouseY <= height / 2 + 50) {
+    state = 2;
+  }
 }
