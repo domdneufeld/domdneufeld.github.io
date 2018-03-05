613c7457a1d1fed37ccc5f0e4a8c9ef3051b394f
domdneufeld <domdneufeld@gmail.com>
2018-03-05 14:34:30 -0600

    sdfsdf

---
+++ dotsandlines/sketch.js
@@ -0,0 +1,23 @@
+// p5js template project - replace with project title
+// Dan Schellenberg - replace with your name
+// Feb 2, 2018 - replace with the date
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  displayDots();
+}
+
+function displayDots() {
+  let pointSpacing = 50;
+  for (let x = pointSpacing; x < width; x += pointSpacing) {
+    for (let y = pointSpacing; y < height; y += pointSpacing) {
+      fill(255);
+      ellipse(x, y, 4, 4);
+      stroke(255, 120);
+      line(x, y, width / 2, height / 2);
+    }
+  }
+}
