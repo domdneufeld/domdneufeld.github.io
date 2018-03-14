e7a82a8451fb89829f5acb1a1b0a55c0d0644191
domdneufeld <domdneufeld@gmail.com>
2018-03-14 14:43:34 -0600

    made clock

--- translationsdemo/sketch.js
+++ translationsdemo/sketch.js
@@ -17,19 +17,23 @@ function draw() {
   // pop();
   // fill(0);
   // rect(100,100,200,200);
+  strokeWeight(3);
+  strokeCap(SQUARE);
   noFill();
   angleMode(DEGREES);
   ellipseMode(CENTER);
   translate(width/2,height/2);
   ellipse(0,0,500);
   fill(0);
+  ellipse(0,0,2);
+
   for (let i = 0; i < 12; i++) {
     rectMode(CENTER);
-    rect(0,-215,10,50);
+    rect(0,-215,3,50);
     rotate(6);
-    // for (let j = 0; j < 4; i++) {
-    //
-    // }
+    for (let j = 0; j < 4; j++) {
+      line(0,-205,0,-240);
+      rotate(6);
+    }
   }
-
 }
